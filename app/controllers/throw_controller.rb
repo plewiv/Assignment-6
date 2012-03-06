63ae4dbb9064d7a8b406f6dddb0aa2d1f240a79b
plewiv <plewiv@cox.net>
2012-03-05 20:02:31 -0800

    Game is working now.

--- app/controllers/throw_controller.rb
+++ app/controllers/throw_controller.rb
@@ -1,11 +1,21 @@
 class ThrowController < ApplicationController
 before_filter do
-	@defeat = {rock: :scissors, paper: :rock, scissors: :paper, rock: :lizard, lizard: :spock, lizard: :paper, spock: :scissors, spock: :rock, scissors: :lizard, paper: :spock}
+	@defeat = {rock: :scissors, paper: :rock, scissors: :paper}
     @throws = @defeat.keys
 	@computer_throw = @throws.sample
 end
   def player_throw
-	
+	@player_throw = params[:type].to_sym
+	if !@throws.include?(@player_throw)
+		halt 403, "You must throw one of the following: #{@throws}"
+	end
+	if @player_throw == @computer_throw
+		@message = "No one is a winner :( TIED!"
+	elsif @computer_throw == @defeat[@player_throw]
+		@message = "You win! Like a BAWS!"
+	else
+		@message = "YOU LOSE SUCKA!"
+	end
   end
 
 end
