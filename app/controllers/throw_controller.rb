13f3ceecc787bc8ca03285bc0d998f4cb01916f6
plewiv <plewiv@cox.net>
2012-03-05 19:40:53 -0800

    initialized the defeat moves and edited the views for throws again

--- app/controllers/throw_controller.rb
+++ app/controllers/throw_controller.rb
@@ -1,5 +1,11 @@
 class ThrowController < ApplicationController
+before_filter do
+	@defeat = {rock: :scissors, paper: :rock, scissors: :paper, rock: :lizard, lizard: :spock, lizard: :paper, spock: :scissors, spock: :rock, scissors: :lizard, paper: :spock}
+    @throws = @defeat.keys
+	@computer_throw = @throws.sample
+end
   def player_throw
+	
   end
 
 end
