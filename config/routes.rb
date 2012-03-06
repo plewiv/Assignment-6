d946d9f574bd1341e93cfebca5119d7976acf218
plewiv <plewiv@cox.net>
2012-03-05 21:24:52 -0800

    clear is working now. add links to all pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Assignment6::Application.routes.draw do
 
-  get "clear/clear"
-
+  match "clear/clear" => 'clear#clear'
   match 'throw/:type' => 'throw#player_throw'
   match 'stats/stats' => 'stats#stats'
 end
