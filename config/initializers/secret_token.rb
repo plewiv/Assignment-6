7fa8d69389182d40d22bf389d4104d11a5996689
plewiv <plewiv@cox.net>
2012-03-05 19:18:22 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Assignment6::Application.config.secret_token = 'f144982683f86b3c5a54cef67c7e1ac031a4ddb5bb4c2705a4fd27ba7eeaa4043846e117a5477a9bb053d75c62b222abd14f2d05699ed6530f4dd8bdd5864775'
